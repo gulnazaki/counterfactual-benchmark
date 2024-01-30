30a1d9659bb4450d69b444c63455ff267b0a26a3
Thomas Melistas <thomas@behavioralsignals.com>
2024-01-30 15:58:41 +0200

    add trabsform to return labels in a dict

--- counterfactual_benchmark/datasets/transforms.py
+++ counterfactual_benchmark/datasets/transforms.py
@@ -7,3 +7,13 @@ class SelectAttributesTransform:
 
     def __call__(self, img, attrs):
         return attrs[[self.attr_idx]], torch.Tensor([attrs[idx] for idx in self.pa_idx])
+
+class ReturnLabelsTransform:
+    def __init__(self, attributes, image_name='image'):
+        self.attributes = attributes
+        self.image_name = image_name
+
+    def __call__(self, img, attrs):
+        label_dict = {attr: value.unsqueeze(dim=-1) for attr, value in zip(self.attributes, attrs)}
+        label_dict[self.image_name] = img
+        return label_dict
