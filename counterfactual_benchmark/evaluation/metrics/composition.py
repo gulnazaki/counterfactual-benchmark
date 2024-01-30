6e180f7142cbcca282862f7f50ff984672a8ced7
nysp78 <you@example.com>
2024-01-30 18:49:25 +0000

    compute composition on unnormalize imgs

--- counterfactual_benchmark/evaluation/metrics/composition.py
+++ counterfactual_benchmark/evaluation/metrics/composition.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
-def composition(factual_batch, method, cycles=10):
+def composition(factual_batch, num_batch, method, cycles=10):
     images = [factual_batch["image"]]
     for _ in range(cycles):
         abducted_noise = method.encode(**factual_batch)
@@ -11,10 +12,22 @@ def composition(factual_batch, method, cycles=10):
 
     # plot all images
     all_images = np.concatenate(images, axis=3)
-    plt.imsave("composition_sample.png", all_images[0][0], cmap='gray')
+    plt.imsave("composition_samples/composition_sample{}.png".format(num_batch), all_images[0][0], cmap='gray')
 
     # loop on different embeddings
     return l1_distance(images, steps=cycles) # add more distances
 
 def l1_distance(all_images, steps):
-    return np.mean(np.abs(all_images[steps].numpy() - all_images[0].numpy()))
+    #compute l1_distance on unnormalize images
+    final_img = unnormalize_image(all_images[steps]).numpy()
+    init_img = unnormalize_image(all_images[0]).numpy()
+
+    return np.mean(np.abs(final_img - init_img))
+
+def unnormalize_image(img):
+    norm_params = torch.load("../../datasets/morphomnist/data/norm_params.pt")
+    img_m = norm_params["img_mean"]
+    img_s = norm_params["img_std"]
+
+    return (img * img_s) + img_m 
+
