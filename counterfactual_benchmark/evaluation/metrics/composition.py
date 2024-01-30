e16b4616ab24140675fde2decf667c06b2ef8d98
Thomas Melistas <thomas@behavioralsignals.com>
2024-01-30 16:01:05 +0200

    change composition to plot sample

--- counterfactual_benchmark/evaluation/metrics/composition.py
+++ counterfactual_benchmark/evaluation/metrics/composition.py
@@ -1,10 +1,20 @@
-def composition(factual_batch, method, parents, cycles=10):
-    for i in range(cycles):
-        abducted_noise = method.encode(factual_batch, parents)
-        counterfactual_batch = method.decode(abducted_noise, parents)
-        factual_batch = counterfactual_batch
-    # loop on different embeddings
-    l1_distance(factual_batch, counterfactual_batch) # add more distances
+import matplotlib.pyplot as plt
+import numpy as np
 
-def l1_distance(factual_batch, counterfactual_batch):
-    pass
+def composition(factual_batch, method, cycles=10):
+    images = [factual_batch["image"]]
+    for _ in range(cycles):
+        abducted_noise = method.encode(**factual_batch)
+        counterfactual_batch = method.decode(**abducted_noise)
+        images.append(counterfactual_batch["image"])
+        factual_batch = counterfactual_batch
+
+    # plot all images
+    all_images = np.concatenate(images, axis=3)
+    plt.imsave("composition_sample.png", all_images[0][0], cmap='gray')
+
+    # loop on different embeddings
+    return l1_distance(images[0], images[-1]) # add more distances
+
+def l1_distance(image1, image2):
+    return np.sum(np.abs(image1 - image2))
