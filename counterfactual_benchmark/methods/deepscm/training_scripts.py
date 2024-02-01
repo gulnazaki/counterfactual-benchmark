86c7adddfd592c0c9855c4242d3786182fa6bb9a
Thomas Melistas <melistas.th@gmail.com>
2024-02-01 19:03:22 +0000

    training changes

--- counterfactual_benchmark/methods/deepscm/training_scripts.py
+++ counterfactual_benchmark/methods/deepscm/training_scripts.py
@@ -6,10 +6,10 @@ sys.path.append("../../")
 from datasets.transforms import SelectAttributesTransform
 from models.utils import generate_checkpoint_callback, generate_early_stopping_callback
 
-def train_flow(flow, config, data_class, graph_structure, attrs, checkpoint_dir, **kwargs):
-    transform = SelectAttributesTransform(attrs.index(flow.name), [attrs.index(attr_pa) for attr_pa in graph_structure[flow.name]])
+def train_flow(flow, config, data_class, graph_structure, attribute_size, checkpoint_dir, **kwargs):
+    transform = SelectAttributesTransform(flow.name, attribute_size, graph_structure)
     # load the data (with continuous labels)
-    data = data_class(attributes=attrs, transform=transform, **kwargs)
+    data = data_class(attribute_size=attribute_size, transform=transform, **kwargs)
     # split into train and validation
     train_set, val_set = torch.utils.data.random_split(data, [config["train_val_split"], 1 - config["train_val_split"]])
     train_data_loader = torch.utils.data.DataLoader(train_set, batch_size=config["batch_size_train"], shuffle=True, num_workers=7)
@@ -22,15 +22,13 @@ def train_flow(flow, config, data_class, graph_structure, attrs, checkpoint_dir,
     trainer.fit(flow, train_data_loader, val_data_loader)
 
 
-def train_vae(vae, config, data_class, graph_structure, attrs, checkpoint_dir, **kwargs):
-    data = data_class(attributes=attrs, **kwargs)
+def train_vae(vae, config, data_class, graph_structure, attribute_size, checkpoint_dir, **kwargs):
+    data = data_class(attribute_size=attribute_size, **kwargs)
 
     # split into train and validation
     train_set, val_set = torch.utils.data.random_split(data, [config["train_val_split"], 1 - config["train_val_split"]])
     train_data_loader = torch.utils.data.DataLoader(train_set, batch_size=config["batch_size_train"], shuffle=True, num_workers=7)
     val_data_loader = torch.utils.data.DataLoader(val_set, batch_size=config["batch_size_val"], shuffle=False, num_workers=7)
-
-    config = {k: (None if v == "null" else v) for k, v in config.items()}
 
     trainer = Trainer(accelerator="auto", devices="auto", strategy="auto", gradient_clip_val=config["gradient_clip_val"],
                       callbacks=[generate_checkpoint_callback(vae.name, checkpoint_dir),
