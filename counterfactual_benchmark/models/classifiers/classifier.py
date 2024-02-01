c1eb9e8d594b71e71cc930acdacf1880d0e2f459
nysp78 <you@example.com>
2024-02-01 16:17:32 +0000

    add effectiveness, classifier

--- counterfactual_benchmark/models/classifiers/classifier.py
+++ counterfactual_benchmark/models/classifiers/classifier.py
@@ -4,13 +4,14 @@ import torch.nn as nn
 import pytorch_lightning as pl
 import torch
 import sys
-#sys.path.append("../../")
-#from datasets.morphomnist.dataset import MorphoMNISTLike 
+sys.path.append("../../")
+from datasets.morphomnist.dataset import MorphoMNISTLike 
 
 class Classifier(pl.LightningModule):
     def __init__(self, attr, in_shape = (1, 28, 28), width=16, num_outputs = 1, context_dim = 0 , lr=1e-3):
         super().__init__()
         self.variable = attr
+        self.lr = lr
         self.variables = {"thickness":0, "intensity":1}
         self.attr = self.variables[attr] #select attribute
         in_channels = in_shape[0]
@@ -53,7 +54,9 @@ class Classifier(pl.LightningModule):
         x = x.mean(dim=(-2, -1))  # avg pooling
 
         if y is not None:
-            y = y.unsqueeze(1)
+
+            #y = y.unsqueeze(1)  
+          #  print(x.shape, y.shape)
             x = torch.cat([x, y], dim=-1)
 
         return self.fc(x)
@@ -64,22 +67,33 @@ class Classifier(pl.LightningModule):
         y = attrs_[:, self.attr] #select attribute to train
 
         if self.variable == "thickness":  #condition on intensity when training the thickness classifier
-            cond = attrs_[:,self.variables["intensity"]]
+            cond = attrs_[:,self.variables["intensity"]].view(-1, 1)
             y_hat = self(x , cond)
         
         else:
             y_hat = self(x)
         
-        loss = nn.BCEWithLogitsLoss()(y_hat, y.type(torch.float32).view(-1, 1))
+        #loss = nn.BCEWithLogitsLoss()(y_hat, y.type(torch.float32).view(-1, 1))
+        loss = nn.MSELoss()(y_hat, y.type(torch.float32).view(-1, 1))
+        self.log("train_loss", loss, on_step=False, on_epoch=True)
+
         #loss = nn.CrossEntropyLoss()(y_hat, y.type(torch.long))
         return loss
     
     def validation_step(self, batch, batch_idx):
         x, attrs_ = batch
         y = attrs_[:, self.attr]
-        y_hat = self(x)
+
+        if self.variable == "thickness":  #condition on intensity when validating the thickness classifier
+            cond = attrs_[:,self.variables["intensity"]].view(-1, 1)  
+            y_hat = self(x , cond)
+
+        else:
+            y_hat = self(x)
         
-        loss = nn.BCEWithLogitsLoss()(y_hat, y.type(torch.float32).view(-1, 1))
+        #loss = nn.BCEWithLogitsLoss()(y_hat, y.type(torch.float32).view(-1, 1))
+        loss = nn.MSELoss()(y_hat, y.type(torch.float32).view(-1, 1))
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
       #  loss = nn.CrossEntropyLoss()(y_hat, y.type(torch.long))
         return loss
     
@@ -88,18 +102,21 @@ class Classifier(pl.LightningModule):
         return optimizer
     
 
-#train_set = MorphoMNISTLike(attributes= ["thickness", "intensity"], train=True, columns= ["thickness", "intensity"])
-#print("Hi")
-#tr_data_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False, num_workers=7)
-#iterator = iter(tr_data_loader)
-#batch = next(iterator)
-#x , attrs = batch
-#print(x.shape)
-#cls_intensity = Classifier(attr="intensity", width=8).eval()
-#cls_thickness = Classifier(attr="thickness", width=8, context_dim=1).eval()
+#just test code for the classifiers
+if __name__ == "__main__":
+    train_set = MorphoMNISTLike(attributes= ["thickness", "intensity"], train=True, columns= ["thickness", "intensity"])
 
-#print(attrs[:,1].shape)
-#out1= cls_intensity(x)
-#out2 = cls_thickness(x, y = attrs[:,1])
-#print(out1.shape)
-#print(out2.shape)
+    tr_data_loader = torch.utils.data.DataLoader(train_set, batch_size=2, shuffle=False, num_workers=7)
+    iterator = iter(tr_data_loader)
+    batch = next(iterator)
+    x , attrs = batch
+    print(x.shape)
+    print(attrs.shape, attrs[:,0].shape)
+    cls_intensity = Classifier(attr="intensity", width=8)
+    cls_thickness = Classifier(attr="thickness", width=8, context_dim=1).eval()
+
+    print(attrs[:,1].shape)
+    out1= cls_intensity(x)
+    out2 = cls_thickness(x, y = attrs[:,1].view(-1, 1))
+    print(out1.shape)
+    print(out2.shape)
