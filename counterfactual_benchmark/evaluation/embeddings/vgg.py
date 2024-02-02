bd4ec63771f660265fb6f785bd77038f2751627f
Thomas Melistas <melistas.th@gmail.com>
2024-02-02 12:28:50 +0000

    fix coverage & density

--- counterfactual_benchmark/evaluation/embeddings/vgg.py
+++ counterfactual_benchmark/evaluation/embeddings/vgg.py
@@ -1,14 +1,12 @@
-from torchvision.models import vgg16
+from torchvision.models import vgg16, VGG16_Weights
 import torch
 
 def vgg(pretrained='False'):
 
-    model=vgg16(pretrained=pretrained)
+    weights = VGG16_Weights.DEFAULT if pretrained else None
+    model=vgg16(weights=weights)
     if torch.cuda.is_available():
         model.to("cuda")
     model.classifier = model.classifier[:-1]
-    
-    return model
 
-    
-    
+    return model
