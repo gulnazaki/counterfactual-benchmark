aba32a7ff9fadc2e6eae79bdc73e523f6a7cc8de
Thomas Melistas <melistas.th@gmail.com>
2024-02-02 11:46:44 +0000

    add unnormalize function for all variables

--- counterfactual_benchmark/evaluation/metrics/composition.py
+++ counterfactual_benchmark/evaluation/metrics/composition.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
+from datasets.morphomnist.dataset import unnormalize
 
 def composition(factual_batch, num_batch, method, cycles=10):
-    images = [unnormalize_image(factual_batch["image"])]
+    images = [unnormalize(factual_batch["image"], name="image")]
     for _ in range(cycles):
         abducted_noise = method.encode(**factual_batch)
         counterfactual_batch = method.decode(**abducted_noise)
-        images.append(unnormalize_image(counterfactual_batch["image"]))
+        images.append(unnormalize(counterfactual_batch["image"], name="image"))
         factual_batch = counterfactual_batch
     # plot all images
     all_images = np.concatenate(images, axis=3)
     plt.imsave("composition_samples/composition_sample{}.png".format(num_batch), all_images[0][0], cmap='gray')
 
-    # loop on different embeddings
+    # TODO loop on different embeddings
     return l1_distance(images, steps=cycles) # add more distances
 
 def l1_distance(images, steps):
@@ -21,12 +21,3 @@ def l1_distance(images, steps):
     init_img = images[0].numpy()
 
     return np.mean(np.abs(final_img - init_img))
-
-def unnormalize_image(img):
-    return (img + 1.0) * 127.5
-    # norm_params = torch.load("../../datasets/morphomnist/data/norm_params.pt")
-    # img_m = norm_params["img_mean"]
-    # img_s = norm_params["img_std"]
-
-    # return (img * img_s) + img_m
-
