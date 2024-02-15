f87c7948b2f1c06a7aa28b7c5979c03a35cabb72
Thomas Melistas <melistas.th@gmail.com>
2024-02-15 10:21:29 +0000

    fix celeba vae sampling

--- counterfactual_benchmark/models/vaes/celeba_vae.py
+++ counterfactual_benchmark/models/vaes/celeba_vae.py
@@ -128,11 +128,15 @@ class DGaussNet(nn.Module):
         )
         return -1.0 * log_probs.mean(dim=(1, 2, 3))
 
-    def sample(self, h):
-        loc, logscale = self.forward(h)
-        x = loc + torch.exp(logscale) * torch.randn_like(loc)
+    def sample(
+        self, h, return_loc: bool = True, t=None):
+        if return_loc:
+            x, logscale = self.forward(h)
+        else:
+            loc, logscale = self.forward(h, t)
+            x = loc + torch.exp(logscale) * torch.randn_like(loc)
         x = torch.clamp(x, min=-1.0, max=1.0)
-        return x
+        return x, logscale.exp()
 
 class CelebaCondVAE(CondVAE):
     def __init__(self, params, attr_size, name="image_vae"):
