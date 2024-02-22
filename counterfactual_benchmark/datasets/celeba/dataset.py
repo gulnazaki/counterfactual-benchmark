33b2401557d99a758920be952708c37b0d78c131
nysp78 <you@example.com>
2024-02-22 15:38:06 +0000

    update hvae architecture

--- counterfactual_benchmark/datasets/celeba/dataset.py
+++ counterfactual_benchmark/datasets/celeba/dataset.py
@@ -8,7 +8,7 @@ MIN_MAX = {
 }
 
 def load_data(data_dir, split):
-    transforms = Compose([CenterCrop(150), Resize((128, 128)), ToTensor(), ConvertImageDtype(dtype=torch.float32),])
+    transforms = Compose([CenterCrop(150), Resize((64, 64)), ToTensor(), ConvertImageDtype(dtype=torch.float32),])
     data = CelebA(root=data_dir, split=split, transform=transforms, download=False)
     return data
 
