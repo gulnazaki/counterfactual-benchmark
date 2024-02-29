1ddd8d38eb291ef4b680b62eab0b0d07cfe8def3
nefeligk <nefgkouti@gmail.com>
2024-02-29 14:25:15 +0000

    add architecture of style generator

--- counterfactual_benchmark/models/gans/celeba_gan.py
+++ counterfactual_benchmark/models/gans/celeba_gan.py
@@ -13,16 +13,9 @@ import numpy as np
 from collections import OrderedDict
 from models.utils import flatten_list, continuous_feature_map
 from models.gans import CondGAN
+import math
+import torch.nn.functional as F
 
-
-
-#Layer F K S BN D A
-#Conv2D 64 (2,2) (1,1) Y 0.0 LReLU
-#Conv2D 128 (7,7) (2,2) Y 0.0 LReLU
-#Conv2D 256 (5,5) (2,2) Y 0.0 LReLU
-#Conv2D 256 (7,7) (2,2) Y 0.0 LReLU
-#Conv2D 512 (4,4) (1,1) Y 0.0 LReLU
-#Conv2D 512 (1,1) (1,1) Y 0.0 Linea
 
 class Encoder(nn.Module):
     def __init__(self, latent_dim, num_continuous, n_chan=[3, 64, 128, 256, 256, 512, 512], stride=[1, 2, 2, 2, 1, 1],
@@ -54,7 +47,7 @@ class Encoder(nn.Module):
 
 
     def forward(self, x: torch.Tensor, cond):
-
+    
 
         attr1 = cond[:, 0]
         attr2 = cond[:, 1]
@@ -62,17 +55,118 @@ class Encoder(nn.Module):
         attr2 = continuous_feature_map(attr2, size=(x.shape[2], x.shape[3]))
 
         features = torch.concat((x, attr1, attr2), dim=1)
+        
         features = self.layers(features)
 
         return features
 
-#Layer F K S BN D A
-#Conv2DT 512 (4,4) (1,1) Y 0.0 LReLU
-#Conv2DT 256 (7,7) (2,2) Y 0.0 LReLU
-#Conv2DT 256 (5,5) (2,2) Y 0.0 LReLU
-#Conv2DT 128 (7,7) (2,2) Y 0.0 LReLU
-#Conv2DT 64 (2,2) (1,1) Y 0.0 LReLU
-#Conv2D 3 (1,1) (1,1) Y 0.0 Sigmoid
+
+class MappingNetwork(nn.Module):
+    def __init__(self, latent_size = 512, hidden_size = 512, num_layers = 8):
+        super(MappingNetwork, self).__init__()
+
+        self.latent_size = latent_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        
+        #layers = [nn.Linear(latent_size, hidden_size)]
+        #for _ in range(num_layers - 1):
+        #    layers += [nn.Linear(hidden_size, hidden_size), nn.ReLU()]
+        #self.mapping = nn.Sequential(*layers)
+
+        self.layers = []
+        self.embs = []
+        for _ in range(num_layers - 2):
+            self.layers.append(nn.Linear(latent_size, hidden_size).to('cuda'))
+            self.embs.append(nn.Embedding(2,512).to('cuda'))
+        self.layers.append(nn.Linear(latent_size, hidden_size).to('cuda'))
+
+    def forward(self, x, cond):
+        x = x.view(-1, self.latent_size).to('cuda')
+        for i in range(self.num_layers - 2):
+        
+            x = self.layers[i](x).to('cuda')
+            c = cond.to('cuda')
+            c = c.matmul(self.embs[i].weight).reshape((-1, 512, 1, 1))
+            c = c.squeeze()
+            x = x+c
+            
+        x = self.layers[-1](x)
+        return x
+     #   x = x.view(-1, self.latent_size)
+      #  return self.mapping(x)
+
+
+class WSLinear(nn.Module):
+    def __init__(
+        self, in_features, out_features,
+    ):
+        super(WSLinear, self).__init__()
+        self.linear = nn.Linear(in_features, out_features)
+        self.scale = (2 / in_features)**0.5
+        self.bias = self.linear.bias
+        self.linear.bias = None
+
+        # initialize linear layer
+        nn.init.normal_(self.linear.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self, x):
+        return self.linear(x * self.scale) + self.bias
+    
+class AdaIN(nn.Module):
+    def __init__(self, channels, w_dim):
+        super().__init__()
+        self.instance_norm = nn.InstanceNorm2d(channels)
+        self.style_scale = WSLinear(w_dim, channels)
+        self.style_bias = WSLinear(w_dim, channels)
+
+    def forward(self, x, w):
+        x = self.instance_norm(x)
+        style_scale = self.style_scale(w).unsqueeze(2).unsqueeze(3)
+        style_bias = self.style_bias(w).unsqueeze(2).unsqueeze(3)
+        return style_scale * x + style_bias    
+class InjectNoise(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.weight = nn.Parameter(torch.zeros(1, channels, 1, 1))
+
+    def forward(self, x):
+        noise = torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]), device=x.device)
+        return x + self.weight * noise
+class WSConv2d(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1
+    ):
+        super(WSConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.scale = (2 / (in_channels * (kernel_size ** 2))) ** 0.5
+        self.bias = self.conv.bias
+        self.conv.bias = None
+
+        # initialize conv layer
+        nn.init.normal_(self.conv.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self, x):
+        return self.conv(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
+    
+class GenBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, w_dim):
+        super(GenBlock, self).__init__()
+        self.conv1 = WSConv2d(in_channels, out_channels)
+        self.conv2 = WSConv2d(out_channels, out_channels)
+        self.leaky = nn.LeakyReLU(0.2, inplace=True)
+        self.inject_noise1 = InjectNoise(out_channels)
+        self.inject_noise2 = InjectNoise(out_channels)
+        self.adain1 = AdaIN(out_channels, w_dim)
+        self.adain2 = AdaIN(out_channels, w_dim)
+
+    def forward(self, x, w):
+        x = self.adain1(self.leaky(self.inject_noise1(self.conv1(x))), w)
+        x = self.adain2(self.leaky(self.inject_noise2(self.conv2(x))), w)
+        return x
+
 class Decoder(nn.Module):
     def __init__(self, latent_dim, num_continuous, n_chan=[512 ,512, 256, 256, 128, 64, 3], stride=[1, 2, 2, 2, 1, 1],
                  kernel_size=[4, 7, 5, 7, 2, 1], padding=[0, 0, 0, 0, 0, 0]):
@@ -85,6 +179,25 @@ class Decoder(nn.Module):
 
 
         activation_fn = nn.LeakyReLU(0.1)
+        self.map = MappingNetwork()
+        
+        self.starting_constant = nn.Parameter(torch.ones((1, 512, 64, 64)))
+        self.initial_adain1 = AdaIN(512, 512)
+        self.initial_adain2 = AdaIN(512, 512)
+        self.initial_noise1 = InjectNoise(512)
+        self.initial_noise2 = InjectNoise(512)
+        self.initial_conv = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.leaky = nn.LeakyReLU(0.2, inplace=True)
+        
+        
+        
+        
+        self.blocks=[]
+        for i in range(9):
+            self.blocks.append(GenBlock(512,512,512).to('cuda'))
+                               
+        self.rgb_layer = WSConv2d(in_channels = 512, out_channels = 3, kernel_size=1, stride=1, padding=0)
+        
         self.layers = nn.Sequential(
             OrderedDict(flatten_list([
                 [('gen' + str(i + 1), nn.ConvTranspose2d(in_channels=n_chan[i], out_channels=n_chan[i + 1],
@@ -94,12 +207,12 @@ class Decoder(nn.Module):
                  ('gen' + str(i + 1) + 'leaky_relu', activation_fn)] for i in range(len(n_chan) - 2)
             ]))
         )
-        lastconv = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=kernel_size[-1], stride=stride[-1], padding=padding[-1])
+        lastconv = nn.Conv2d(in_channels=512, out_channels=3, kernel_size=kernel_size[-1], stride=stride[-1], padding=padding[-1])
         batchnorm = nn.BatchNorm2d(3)
-        sig = nn.Sigmoid()
+        self.sig = nn.Sigmoid()
         self.layers.append(lastconv)
         self.layers.append(batchnorm)
-        self.layers.append(sig)
+        self.layers.append(self.sig)
 
     def forward(self, u, cond):
 
@@ -108,12 +221,21 @@ class Decoder(nn.Module):
         attr1 = continuous_feature_map(attr1, size=(1, 1))
         attr2 = continuous_feature_map(attr2, size=(1, 1))
 
-
-        features = torch.concat((u, attr1, attr2), dim=1)
-
-        features = self.layers(features)
-
-        return features
+        
+        #w = self.map(noise)
+        w = self.map(u, cond)
+        x = self.initial_adain1(self.initial_noise1(self.starting_constant), w)
+        x = self.initial_conv(x)
+        out = self.initial_adain2(self.leaky(self.initial_noise2(x)), w)
+        
+        
+        #upscaled = F.interpolate(out, scale_factor=2, mode="bilinear")
+        for i in range(9):
+            out = self.blocks[i](out, w)
+        out = self.rgb_layer(out)
+        out = self.sig(out)
+    
+        return out
 
 
 class Discriminator(nn.Module):
