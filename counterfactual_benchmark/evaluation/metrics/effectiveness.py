259069dd7d29492483d40b3da2ed1dd10532f361
nysp78 <you@example.com>
2024-03-03 21:53:19 +0000

    add new eyeglasses checkpoint

--- counterfactual_benchmark/evaluation/metrics/effectiveness.py
+++ counterfactual_benchmark/evaluation/metrics/effectiveness.py
@@ -1,18 +1,17 @@
-from torchmetrics.classification import BinaryAccuracy, BinaryF1Score
-
+from torchmetrics.classification import BinaryAccuracy, BinaryF1Score, BinaryRecall, BinaryPrecision
+import torch.nn as nn
 def effectiveness(counterfactual_batch, unnormalize_fn, predictors):
    # print(counterfactual_batch["intensity"].shape)
     targets = {key: value for key , value in counterfactual_batch.items() if key!="image"} #select the counterfactual  parents
     predictions = {key: clfs(counterfactual_batch["image"], counterfactual_batch["intensity"])
                    if key=="thickness"
-                   else clfs(counterfactual_batch["image"]) for key , clfs in predictors.items()} #predicted values
-
+                   else nn.Sigmoid()(clfs(counterfactual_batch["image"])) for key , clfs in predictors.items()} #predicted values
+   # print(predictions, targets)
     if "digit" in list(targets.keys()):
         result = {key:(unnormalize_fn(targets[key], key) - unnormalize_fn(predictions[key], key)).abs().mean().detach().cpu().numpy()
               if key!="digit" else  (targets[key].argmax(-1).cpu().numpy() == predictions[key].argmax(-1).cpu().numpy()).mean()
               for key in targets}
 
     else: #celeba attributes
-        result = {key: BinaryF1Score(threshold=0.5)(predictions[key].detach().cpu(), targets[key].detach().cpu()) for key in targets}
-
+        result = {key: BinaryF1Score()(predictions[key].detach().cpu(), targets[key].detach().cpu()) for key in targets}
     return result
