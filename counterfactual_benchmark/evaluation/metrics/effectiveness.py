f257ed5db1872ad94b42820d04cc5609ce8cea4f
Thomas Melistas <thomas@behavioralsignals.com>
2024-03-14 19:08:18 +0200

    detach effectiveness tensor

--- counterfactual_benchmark/evaluation/metrics/effectiveness.py
+++ counterfactual_benchmark/evaluation/metrics/effectiveness.py
@@ -1,4 +1,4 @@
-from torchmetrics.classification import BinaryAccuracy, BinaryF1Score, BinaryRecall, BinaryPrecision
+from torchmetrics.classification import BinaryF1Score
 import torch.nn as nn
 
 def effectiveness(counterfactual_batch, unnormalize_fn, predictors):
@@ -9,8 +9,8 @@ def effectiveness(counterfactual_batch, unnormalize_fn, predictors):
                    else nn.Sigmoid()(clfs(counterfactual_batch["image"])) for key , clfs in predictors.items()} #predicted values
 
     if "digit" in list(targets.keys()):
-        result = {key:(unnormalize_fn(targets[key], key) - unnormalize_fn(predictions[key], key)).abs().mean().cpu().numpy()
-              if key!="digit" else  (targets[key].argmax(-1) == predictions[key].argmax(-1)).sum().cpu().numpy() / predictions[key].shape[0]
+        result = {key:(unnormalize_fn(targets[key], key) - unnormalize_fn(predictions[key], key)).abs().mean().detach().cpu().numpy()
+              if key!="digit" else  (targets[key].argmax(-1) == predictions[key].argmax(-1)).sum().detach().cpu().numpy() / predictions[key].shape[0]
               for key in targets}
 
     else: #celeba attributes
